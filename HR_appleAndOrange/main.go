cfc442a63c23462a35cd3f429fb64b954cabea0c
Ilyin Nikolay <irishwithaxe@gmail.com>
2017-05-19 22:39:15 +0300

    apples and oranges done

--- HR_appleAndOrange/main.go
+++ HR_appleAndOrange/main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("asdsd")
+	var s, t, a, b, napples, noranges, d, fallsapples, fallsoranges int
+
+	fmt.Scanf("%d %d", &s, &t)
+	fmt.Scanf("%d %d", &a, &b)
+	fmt.Scanf("%d %d", &napples, &noranges)
+
+	for i := 0; i < napples; i++ {
+		fmt.Scanf("%d", &d)
+		var dist = a + d
+		if dist >= s && dist <= t {
+			fallsapples++
+		}
+	}
+	for i := 0; i < noranges; i++ {
+		fmt.Scanf("%d", &d)
+		var dist = b + d
+		if dist >= s && dist <= t {
+			fallsoranges++
+		}
+	}
+
+	fmt.Println(fallsapples)
+	fmt.Println(fallsoranges)
 }
