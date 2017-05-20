d7caf6f965cf1f1e16b6912623c742ad587dd2fa
Ilyin Nikolay <irishwithaxe@gmail.com>
2017-05-20 12:25:20 +0300

    done kangaroo

--- HR_Kangaroo/main.go
+++ HR_Kangaroo/main.go
@@ -1,24 +1,35 @@
 package main
 
 import "fmt"
-import "math"
+
+// test29 input 1571 4240 9023 4234 	-> YES
+// test10 input 43 2 70 2			-> NO
 
 func main() {
-	var x1, x2, v1, v2 float64
+	var x1, v1, x2, v2 float32
 	fmt.Scanf("%f %f %f %f", &x1, &v1, &x2, &v2)
 
-	var oldDistance, newDistance = 0.0, math.Abs(x1 - x2)
+	if x1 > x2 {
+		x1, v1, x2, v2 = x2, v2, x1, v1
+	}
+
+	if x1 == x2 {
+		fmt.Println("YES")
+		return
+	}
+
+	if (x2-x1) < (x2+v2-x1-v1) || v1 == v2 {
+		fmt.Println("NO")
+		return
+	}
 
 	for {
-		oldDistance = newDistance
-		newDistance = math.Abs(x1 - x2)
-
-		if oldDistance < newDistance {
+		if x1 > x2 {
 			fmt.Println("NO")
 			return
 		}
 
-		if newDistance == 0 {
+		if x1 == x2 {
 			fmt.Println("YES")
 			return
 		}
